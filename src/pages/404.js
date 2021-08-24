7294258fac0c66da3068d96243dad2292d68bcac
Andrew Nowacki <andrew.p.nowacki@gmail.com>
2021-08-24 14:39:42 -0400

    testing

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,19 +1,20 @@
 import * as React from "react"
 import { graphql } from "gatsby"
 
-import Layout from "../components/layout"
+// import Layout from "../components/layout"
 import Seo from "../components/seo"
 
-const NotFoundPage = ({ data, location }) => {
-  const siteTitle = data.site.siteMetadata.title
-
+const NotFoundPage = ({ data }) => {
+  // const siteTitle = data.site.siteMetadata.title
 
   return (
-    <Layout title={siteTitle} >
+    // <Layout title={siteTitle} >
+    <div>
       <Seo title="404: Not Found" />
       <h1>404: Not Found</h1>
       <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
-    </Layout>
+    </div>
+    // </Layout>
   )
 }
 
