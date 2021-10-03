9d1e54599c03f187e3623dded5cbffba2920dddc
Andrew Nowacki <andrew.p.nowacki@gmail.com>
2021-10-03 19:20:24 -0400

    uploading

--- src/components/SideScroll/ourPillars.js
+++ src/components/SideScroll/ourPillars.js
@@ -13,16 +13,11 @@ import { useInView } from "react-intersection-observer"
 import { useEmblaCarousel } from "embla-carousel/react"
 
 const OurPillars = () => {
-
-
-  //+++++++++++++++++++++ Horizontal Scroll Logic +++++++++++++++++++++ */
-// This component handles the side scrolling "Pillars" section.
-  // The height of the container (StickyContainer) defines how long the user must scroll to get to the next slide
-
   // ------------------------- 1. Establish refs -------------------------
   const ref = useRef()
   const horizontalScroll = useRef()
 
+  // refs for each slide to animate when inView
   const [StudioRef, StudioRefInView] = useInView({
     root: null,
     threshold: 0.6,
@@ -52,105 +47,7 @@ const OurPillars = () => {
     [StudioRef, JamsRef, CollabRef]
   )
 
-  // ------------------- 2. Data for slide markup -------------------
-  const SideScrollData = [
-    {
-      ref: StudioRef,
-      inView: StudioRefInView,
-      title: "Wonder Works Studio",
-      titleColor: "#D9E141",
-      bodyText:
-        "Discover what’s in the works at Wonder Works Studio. We’re always dreaming up new adventures in exciting roleplay games for immersive, imaginative fun for everyone. Check out our ambitious new projects or our latest launches—they all live here. ",
-      headerSVG: <Svg.WWStudioHeader />,
-      mainSVG: <Svg.WWStudioMain />,
-      rightSVG: <Svg.WWStudioRight />,
-    },
-    {
-      ref: JamsRef,
-      inView: JamsRefInView,
-      title: "Wonder Works Jams",
-      titleColor: "#F9DB1E",
-      bodyText:
-        "Wonder Works Jams is a space for our junior talent to QA various game genres. It’s a creative hub of mentorship that fosters a lifelong love for exploration and innovation and promotes success on individual and collaborative levels. ",
-      headerSVG: <Svg.WWJamsHeader />,
-      mainSVG: <Svg.WWJamsMain />,
-      rightSVG: <Svg.WWJamsRight />,
-    },
-    {
-      ref: CollabRef,
-      inView: CollabRefInView,
-      title: "Wonder Works Collab",
-      titleColor: "#1A1749",
-      bodyText:
-        "Discover what’s in the works at Wonder Works Studio. We’re always dreaming up new adventures in exciting roleplay games for immersive, imaginative fun for everyone. Check out our ambitious new projects or our latest launches—they all live here. ",
-      headerSVG: <Svg.WWCollabHeader />,
-      mainSVG: <Svg.WWCollabMain />,
-      rightSVG: <Svg.WWCollabRight />,
-    },
-  ]
-
-  // ------------------- 3. Track component scroll progress -------------------
-
-  // Hook which returns a value between 1-0 depending on how close the bottom of the container is to the top of the viewport. When scrollProgress = 0, the container's bottom is touching top of viewport.
-  const useScrollProgress = () => {
-    const [scrollProgress, setScrollProgress] = useState()
-    useLayoutEffect(() => {
-      const onScroll = () => {
-        // get the component's coordinates
-        const horizontalScrollDiv =
-          horizontalScroll.current.getBoundingClientRect()
-          // divide bottom distance to top of viewport by component height to get a 1-0 value of scroll progress
-        setScrollProgress(
-          horizontalScrollDiv.bottom / horizontalScrollDiv.height
-        )
-      }
-      window.addEventListener("scroll", onScroll)
-      return () => window.removeEventListener("scroll", onScroll)
-    }, [])
-    return scrollProgress
-  }
-  const scrollProgress = useScrollProgress()
-
-  // ------------------- 4. Embla Carousel Logic -------------------
-  // Configure Embla settings (notably: disabled user touch/click interaction)
-  const [viewportRef, embla] = useEmblaCarousel({
-    skipSnaps: false,
-    draggable: false,
-    align: "start",
-    speed: 2.5,
-  })
-
-  // Create callback hooks for scrolling each slide into view according to its index
-  const scrollToFirstSlide = useCallback(
-    () => embla && embla.scrollTo(0),
-    [embla]
-  )
-  const scrollToSecondSlide = useCallback(
-    () => embla && embla.scrollTo(1),
-    [embla]
-  )
-  const scrollToThirdSlide = useCallback(
-    () => embla && embla.scrollTo(2),
-    [embla]
-  )
-
-  // Create rules for when the slides should change according to the returned value from scrollProgress
-  useEffect(() => {
-    if (scrollProgress > 0.8 && scrollProgress < 1) {
-      scrollToFirstSlide()
-    } else if (scrollProgress < 0.8 && scrollProgress > 0.5) {
-      scrollToSecondSlide()
-    } else if (scrollProgress < 0.5 && scrollProgress > 0) {
-      scrollToThirdSlide()
-    }
-  }, [scrollProgress])
-
-  // Run Embla
-  useEffect(() => {
-    if (!embla) return
-  }, [embla])
-
-  // ------------------- 5. Framer animation variants -------------------
+  // ------------------- 2. Framer animation variants -------------------
   const sideScrollHeader = {
     visible: {
       opacity: 1,
@@ -222,10 +119,110 @@ const OurPillars = () => {
     },
   }
 
-  //+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ */
+  // ------------------- 3. Track component scroll progress -------------------
+  // This section handles the side scrolling "Pillars" section.
+  // The height of the container (StickyContainer) defines how long the user must scroll to get to the next slide
 
+  // Hook which returns a value between 1-0 depending on how close the bottom of the container is to the top of the viewport. When scrollProgress = 0, the container's bottom is touching top of viewport.
+  const useScrollProgress = () => {
+    const [scrollProgress, setScrollProgress] = useState()
+    useLayoutEffect(() => {
+      const onScroll = () => {
+        // get the component's coordinates
+        const horizontalScrollDiv =
+          horizontalScroll.current.getBoundingClientRect()
+        // divide bottom distance to top of viewport by component height to get a 1-0 value of scroll progress
+        setScrollProgress(
+          horizontalScrollDiv.bottom / horizontalScrollDiv.height
+        )
+      }
+      window.addEventListener("scroll", onScroll)
+      return () => window.removeEventListener("scroll", onScroll)
+    }, [])
+    return scrollProgress
+  }
+  const scrollProgress = useScrollProgress()
+
+  // ------------------- 4. Embla Carousel Logic -------------------
+  // Configure Embla settings (notably: disabled user touch/click interaction)
+  const [viewportRef, embla] = useEmblaCarousel({
+    skipSnaps: false,
+    draggable: false,
+    align: "start",
+    speed: 2.5,
+  })
+
+  // Create callback hooks for scrolling each slide into view according to its index
+  const scrollToFirstSlide = useCallback(
+    () => embla && embla.scrollTo(0),
+    [embla]
+  )
+  const scrollToSecondSlide = useCallback(
+    () => embla && embla.scrollTo(1),
+    [embla]
+  )
+  const scrollToThirdSlide = useCallback(
+    () => embla && embla.scrollTo(2),
+    [embla]
+  )
+
+  // Create rules for when the slides should change according to the returned value from scrollProgress
+  useEffect(() => {
+    if (scrollProgress > 0.8 && scrollProgress < 1) {
+      scrollToFirstSlide()
+    } else if (scrollProgress < 0.8 && scrollProgress > 0.4) {
+      scrollToSecondSlide()
+    } else if (scrollProgress < 0.4 && scrollProgress > 0) {
+      scrollToThirdSlide()
+    }
+  }, [scrollProgress])
+
+  // Run Embla
+  useEffect(() => {
+    if (!embla) return
+  }, [embla])
+
+  // ------------------- 5. Data for slide markup -------------------
+  const SideScrollData = [
+    {
+      ref: StudioRef,
+      inView: StudioRefInView,
+      title: "Wonder Works Studio",
+      titleColor: "#D9E141",
+      bodyText:
+        "Discover what’s in the works at Wonder Works Studio. We’re always dreaming up new adventures in exciting roleplay games for immersive, imaginative fun for everyone. Check out our ambitious new projects or our latest launches—they all live here. ",
+      headerSVG: <Svg.WWStudioHeader />,
+      mainSVG: <Svg.WWStudioMain />,
+      rightSVG: <Svg.WWStudioRight />,
+    },
+    {
+      ref: JamsRef,
+      inView: JamsRefInView,
+      title: "Wonder Works Jams",
+      titleColor: "#F9DB1E",
+      bodyText:
+        "Wonder Works Jams is a space for our junior talent to QA various game genres. It’s a creative hub of mentorship that fosters a lifelong love for exploration and innovation and promotes success on individual and collaborative levels. ",
+      headerSVG: <Svg.WWJamsHeader />,
+      mainSVG: <Svg.WWJamsMain />,
+      rightSVG: <Svg.WWJamsRight />,
+    },
+    {
+      ref: CollabRef,
+      inView: CollabRefInView,
+      title: "Wonder Works Collab",
+      titleColor: "#1A1749",
+      bodyText:
+        "Discover what’s in the works at Wonder Works Studio. We’re always dreaming up new adventures in exciting roleplay games for immersive, imaginative fun for everyone. Check out our ambitious new projects or our latest launches—they all live here. ",
+      headerSVG: <Svg.WWCollabHeader />,
+      mainSVG: <Svg.WWCollabMain />,
+      rightSVG: <Svg.WWCollabRight />,
+    },
+  ]
   return (
     <StickyContainer ref={horizontalScroll}>
+      <Fixed>
+        <h2>{scrollProgress}</h2>
+      </Fixed>
       <Embla>
         <EmblaViewport ref={viewportRef}>
           <EmblaContainer>
@@ -297,6 +294,16 @@ const OurPillars = () => {
 }
 
 export default OurPillars
+
+const Fixed = styled.div`
+  position: fixed;
+  z-index: 9999;
+  top: 20%;
+  left: 20%;
+  h2 {
+    color: red;
+  }
+`
 
 // the height value here determines the "length" of the horizontal scroll carousel
 // higher values = longer distance to initiate slide change
