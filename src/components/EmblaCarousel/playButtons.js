d18d6a2c77c782899d6430a5142d8de6e8376b62
Andrew Nowacki <andrew.p.nowacki@gmail.com>
2021-10-18 18:27:06 -0400

    upload for testing

--- src/components/EmblaCarousel/playButtons.js
+++ src/components/EmblaCarousel/playButtons.js
@@ -56,7 +56,7 @@ export const PlayButtonProjectsPage = ({
   }
 
   return (
-    <AnimatePresence>
+    <EmblaChild>
       {/* Desktop layout starts here */}
       <Background key="desktop" onClick={handleClick}>
         <BackgroundText>
@@ -78,7 +78,7 @@ export const PlayButtonProjectsPage = ({
           placeholder="none"
           quality={100}
           style={{ position: "relative", zIndex: "5" }}
-          />
+        />
       </ThumbnailWrapper>
 
       {/* Mobile layout shift starts here */}
@@ -89,7 +89,7 @@ export const PlayButtonProjectsPage = ({
           whileTap={{ scale: 0.9 }}
           variants={button}
           initial="visible"
-          >
+        >
           <TextWrapper variants={rotation} animate="visible">
             {!paused ? <PressPauseSVG /> : <PressPlaySVG />}
           </TextWrapper>
@@ -100,11 +100,11 @@ export const PlayButtonProjectsPage = ({
               viewBox="0 0 78 89"
               fill="none"
               xmlns="http://www.w3.org/2000/svg"
-              >
+            >
               <path
                 d="M0 8.70153C0 3.89553 3.86997 0 8.64554 0C10.1286 0 11.0687 0.38982 12.7225 1.05198L73.3695 36.563C76.1897 38.2451 77.4959 40.948 78 44.0639V44.9379C77.4959 48.052 76.1897 50.7558 73.3695 52.4388L12.7233 87.9489C11.0687 88.612 10.1286 89 8.64643 89C3.87086 89 0.0008843 85.1045 0.0008843 80.2985L0 8.70153Z"
                 fill="#F7F7FC"
-                />
+              />
             </svg>
           </PlaySVG>
         </Playbutton>
@@ -119,9 +119,14 @@ export const PlayButtonProjectsPage = ({
           />
         </ThumbnailWrapper>
       </MobileThumbnail>
-    </AnimatePresence>
+    </EmblaChild>
   )
 }
+
+const EmblaChild = styled.div`
+  width: 100%;
+  height: 100%;
+`
 
 const Background = styled(motion.div)`
   width: 100%;
