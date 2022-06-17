a70d3a53f551eb7f71ad2736c6b0c6946a278d98
Andrew Nowacki <andrew.p.nowacki@gmail.com>
2022-06-17 13:38:03 -0400

    uploading network fixes

--- gatsby-config.js
+++ gatsby-config.js
@@ -31,7 +31,6 @@ module.exports = {
     //     // gtmAuth: "YOUR_GOOGLE_TAGMANAGER_ENVIRONMENT_AUTH_STRING",
     //     // gtmPreview: "YOUR_GOOGLE_TAGMANAGER_ENVIRONMENT_PREVIEW_NAME",
     //     // dataLayerName: "YOUR_DATA_LAYER_NAME",
-
     //     // Defaults to false
     //     // enableWebVitalsTracking: true,
     //     // Defaults to https://www.googletagmanager.com
